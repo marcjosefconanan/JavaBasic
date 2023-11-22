f448e612a1000a050ccd50601d754334fcc8350d
Marc Josef Conanan <116675081+marcjosefconanan@users.noreply.github.com>
2023-11-22 23:02:00 +0100

    Delete JavaBasic/src/Array21.java

--- JavaBasic/src/Array21.java
+++
@@ -1,3 +0,0 @@
-//Scrivi un programma che contenga un metodo che crea un array e lo riempie con i numeri da 1 a 10 e che che calcoli la somma dei numeri contenuti nell'array inizializzato e la stampi a video.
-public class Array21 {
-}
