87d1f520b0f51e09444017cb5e8853f7f33c71b0
Marc Josef Conanan <116675081+marcjosefconanan@users.noreply.github.com>
2023-11-22 23:02:38 +0100

    Delete JavaBasic/src/Multidimensionali24.java

--- JavaBasic/src/Multidimensionali24.java
+++
@@ -1,3 +0,0 @@
-//Scrivere un programma che contenga un metodo che permette di inizializzare una matrice riempita con dei valori a vostro piacimento e restituisca la somma degli elementi sulla prima riga
-public class Multidimensionali24 {
-}
